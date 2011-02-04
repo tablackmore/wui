9833a44b37f4c9ab6427afbb9144e63492b9af6d
Tom Blackmore <tom@arctictiger.se>
2011-02-04 17:54:03 +0100

    I've added an app control to house the whole app in

--- src/js/wui_controls_app.js
+++ src/js/wui_controls_app.js
@@ -6,16 +6,11 @@
  *  wui_controls_control.js
  * 
  *  wui.css
- *  target/controls/wui_panel.css
  */
-wui.controls.panel = function() {
+wui.controls.app = function() {
     var that = wui.controls.control();
-    that.css.addClass("wui_controls_panel");
     that.css.addClass("wui_position_vbox");
-    that.css.addClass("wui_position_flex");
-    that.setText = function(text) {
-        that.getDomElement().innerHTML = text;
-    };
+    that.css.addClass("wui_position_fullscreen");
     that.show = function() {
         that.getDomElement().style.display = '-webkit-box';
     };
