74f528c1ebbd2d0c17075e890cbda94a0b578f53
Tom Blackmore <tablackmore@gmail.com>
2013-12-05 11:26:12 +0100

    Small tidy of the demo code

--- demo/js/gsnamnd.controls.infoPanel.js
+++ demo/js/gsnamnd.controls.infoPanel.js
@@ -1,23 +1,16 @@
 gsnamnd.controls.infoPanel = function () {
-	var that = wui.controls.panels.standard();
-	var i;
+	var i, that = wui.controls.panels.standard();
 	that.titlebar = wui.controls.titleBar();
 	that.setInfo = function (id) {
 		that.titlebar.setText(id);
 	};
-
 	that.mainPanel = wui.controls.panels.scroll();
-
-	that.mainPanel.getDomElement();
 	that.back = wui.controls.buttons.back();
 	that.back.setText("back");
-
 	that.edit = wui.controls.buttons.standard();
 	that.edit.setText("Edit");
-
 	that.titlebar.leftContainer.appendControl(that.back);
 	that.titlebar.rightContainer.appendControl(that.edit);
-
 	that.list = wui.controls.lists.rounded();
 	var addListItem = function () {
 		var listItem = wui.controls.lists.items.standard();
@@ -27,11 +20,11 @@ gsnamnd.controls.infoPanel = function () {
 		});
 		that.list.items.add(listItem);
 	};
+	//Add 20 items to the list
 	for (i = 0; i < 20; i++) {
 		addListItem();
 	}
 	that.mainPanel.appendControl(that.list);
-
 	that.appendControl(that.titlebar);
 	that.appendControl(that.mainPanel);
 	return that;
--- demo/js/gsnamnd.controls.listPanel.js
+++ demo/js/gsnamnd.controls.listPanel.js
@@ -3,11 +3,9 @@ gsnamnd.controls.listPanel = function () {
 		that = wui.controls.panels.standard(),
 		infoPanel = gsnamnd.controls.infoPanel();
 	infoPanel.hide();
-
 	that.titlebar = wui.controls.titleBar();
 	that.titlebar.setText("List");
 	that.mainPanel = wui.controls.panels.scroll();
-
 	that.mainPanel.getDomElement();
 	that.addButton = wui.controls.buttons.add();
 	that.addButton.setText("Add");
@@ -15,9 +13,7 @@ gsnamnd.controls.listPanel = function () {
 		addListItem(i);
 		i++;
 	});
-
 	that.titlebar.leftContainer.appendControl(that.addButton);
-
 	that.list = wui.controls.lists.standard();
 	var addListItem = function (i) {
 		var listItem = wui.controls.lists.items.standard();
@@ -39,7 +35,6 @@ gsnamnd.controls.listPanel = function () {
 		addListItem(i);
 	}
 	that.mainPanel.appendControl(that.list);
-
 	that.appendControl(that.titlebar);
 	that.appendControl(that.mainPanel);
 	that.appendControl(infoPanel);
